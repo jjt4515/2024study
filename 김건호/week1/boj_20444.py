b702eaf0692b9b7ad8684270e1ff569872fd3c84
amm0124 <108533909+amm0124@users.noreply.github.com>
2024-01-09 13:58:48 +0900

    Update boj_20444.py

--- 김건호/week1/boj_20444.py
+++ 김건호/week1/boj_20444.py
@@ -5,10 +5,9 @@ d=(n+2)**2-4*k
 if d<0 :
     print("NO")
 else : #d>=0
-    if (d**0.5).is_integer(): #--error point--
-        d=(d**0.5)
-        sol1=((n+2)+d)/2
-        sol2=((n+2)-d)/2
+    if int(d**0.5)**2==d:
+        sol1=((n+2)+d**(0.5))/2
+        sol2=((n+2)-d**(0.5))/2
         if sol1>=1 and sol2>=1 :
             if sol1.is_integer() and sol2.is_integer():
                 print("YES")
